9d94801fbe0d1ae754255ff9d9d287aa05dba74a
Michel Massamiri <massamiri.michel@gmail.com>
2018-02-13 16:44:31 +0100

    Fixing JUnit Test Version

--- refactoring/src/model/CustomersTest.java
+++ refactoring/src/model/CustomersTest.java
@@ -1,8 +1,8 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class CustomersTest {
 
